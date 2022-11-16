7afa7110ca211689c91f885649919ea1ed65ea18
Aviv Faraj <avivfaraj@Avivs-iMac.local>
2022-11-16 14:38:15 -0500

    Added code to make it compatible with both Mac, Linux and Windows

--- src/acquisition.py
+++ src/acquisition.py
@@ -8,6 +8,18 @@ Created on Thu Oct  6 19:41:03 2022
 import pandas as pd
 from sodapy import Socrata
 import os
+import sys
+
+
+# Define directories separation character based on OS
+# Values were found in https://stackoverflow.com/a/13874620
+# Darwin - MacOS
+if sys.platform in ['darwin', 'linux']: 
+    dir_sep = '/'
+
+# Windows, Windows/cygwin, Windows/MSYS2
+elif sys.platform in ['win32', 'msys', 'cygwin']: 
+    dir_sep = '\\'
 
 # Unauthenticated client only works with public data sets. Note 'None'
 # in place of application token, and no username or password:
@@ -19,8 +31,7 @@ def get_data(client, dataset_key, overwrite=False):
     name = metadata['name'].replace(" ", "_")
     print(name)
 
-    path = f"..\\data\\Pulled\\{name}.csv"
-    # path = f"..\\data\\{name}.csv"
+    path = f"..{dir_sep}data{dir_sep}Pulled\\{name}.csv"
     if not os.path.isfile(path) or overwrite:
         results = client.get_all(id)
 
@@ -57,33 +68,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-    # First 2000 results, returned as JSON from API / converted to Python list of
-    # dictionaries by sodapy.
-    # for key in list(pa_datasets.keys()):
-    #     id = pa_datasets[key]
-    #     metadata = client.get_metadata(id)
-    #     name = metadata['name'].replace(" ", "_")
-    #     path = f"..\\data\\from_api\\{name}.csv"
-    #     # path = f"..\\data\\{name}.csv"
-    #     if not os.path.isfile(path):
-    #         results = client.get_all(id)
-    #         # results = client.get(id)
-    #         # Convert to pandas DataFrame
-    #         results_df = pd.DataFrame.from_records(results)
-    #         results_df.to_csv(path)
-
-    # for key in pa_datasets.keys():
-    #     id = pa_datasets[key]
-    #     metadata = client.get_metadata(id)
-    #     name = metadata['name'].replace(" ", "_")
-    #     path = f"..\\data\\from_api\\{name}.csv"
-    #     # path = f"..\\data\\{name}.csv"
-    #     df = pd.read_csv(path)
-    #     print(len(df))
-
-    # dataset_keys = {'drugbusts': 'wmgc-6qvd', 'naloxone': 'wst4-3int', 'riskyprescribing': '7m4f-4k58',
-    #                'drugtakeback': 'vjk8-em4w', 'overdoseinfonet': 'hbkk-dwy3', 'dispensation': 'rr54-ur6z',
-    #                'unemployment': 'rqq6-7e5m', 'treatmentfacilities': 'eswt-bam9', 'druguseestimates': 'krjw-t48p'}
