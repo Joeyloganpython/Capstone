b2dfcdee49040aee1bdb6ae9fa098cd531d16a9a
Aviv Faraj <avivfaraj@Avivs-iMac.local>
2022-11-27 18:43:33 -0500

    Added new dashboards to website

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,18 +1,34 @@
 let buttons =[
   {
-    name: "County Level Dashboard",
+    name: "County Level",
     filename: "index.html",
   },
   {
-    name: "State Level Dashboard",
+    name: "State Level",
     filename: "state_level.html",
   },
   {
-    name: "Survival Rate Dashboard",
+    name: "Survival Rate",
     filename: "survival_rate_wo_naloxone.html"
   },
   {
-    name: "Risky Prescription Story",
+    name: "Incident Count",
+    filename: "overdose_per_10000.html"
+  },
+  {
+    name: "Day Analysis",
+    filename: "day_and_time.html"
+  },
+  {
+    name: "Time Analysis",
+    filename: "overdose_incident_time.html"
+  },
+  {
+    name: "Month Analysis",
+    filename: "incidents_monthly.html"
+  },
+  {
+    name: "Risky Prescription",
     filename: "risky_opioid_prescriptions_pa.html"
   }
 ]
